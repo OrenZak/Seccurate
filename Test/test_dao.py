b40022a752ca7c9d44431f4aacb003ecc635252b
guyshakk <guyshakk@gmail.com>
2019-10-15 10:04:58 +0300

    deleting unnecessary files

--- Test/test_dao.py
+++ Test/test_dao.py
@@ -148,6 +148,78 @@ class TestRXSSCRUD(unittest.TestCase):
     def suite(self):
         pass
 
+class TestSQLICRUD(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.__SQLICRUD = SQLICrud.getInstance('D:\DB\TestsqliPayloads.db')
+        cls.__vulnDescriptor = VulnerabilityDescriptionCRUD.getInstance('D:\DB\TestVulnsDescription.db')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.__SQLICRUD.closeConnection()
+        cls.__SQLICRUD = None
+        cls.__vulnDescriptor = None #vulnDescriptor will only be closed by the last class using it to avoid programming error
+
+    def setUp(self):
+        vuln1 = VulnerabilityDescriptionObject(name='Testname1', severity=1, description='abc', recommendations='aaa')
+        vuln2 = VulnerabilityDescriptionObject(name='Testname2', severity=2, description='def', recommendations='bbb')
+        self.__vulnDescriptor.createVulnerabilityDescription(vuln1)
+        self.__vulnDescriptor.createVulnerabilityDescription(vuln2)
+        sqli1 = SQLIPayloadEntity(payload='abcTest', type='type1', vuln_descriptor=1)
+        sqli2 = SQLIPayloadEntity(payload='defTest', type='type2', vuln_descriptor=2)
+        self.__SQLICRUD.createPayload(sqli1)
+        self.__SQLICRUD.createPayload(sqli2)
+
+    def tearDown(self):
+        self.__SQLICRUD.deleteAllDataFromTable()
+        self.__vulnDescriptor.deleteAllDataFromTable()
+
+    def test_create_payload(self):
+        self.assertEqual('abcTest', self.__SQLICRUD.getSQLIPayloads(1,0)[0].getPayload())
+        self.assertEqual('defTest', self.__SQLICRUD.getSQLIPayloads(1,1)[0].getPayload())
+
+    def test_wrong_create_payload(self):
+        self.assertNotEqual('abdTest', self.__SQLICRUD.getSQLIPayloads(1,0)[0].getPayload())
+
+    def test_get_payloads_pagination(self):
+        self.assertEqual(len(self.__SQLICRUD.getSQLIPayloads()), 2)
+
+    def test_read_by_id(self):
+        self.assertEqual('abcTest', self.__SQLICRUD.getPayloadByID(1).getPayload())
+        self.assertEqual('defTest', self.__SQLICRUD.getPayloadByID(2).getPayload())
+
+    def test_wrong_read_by_id(self):
+        with self.assertRaises(Exception):
+            self.__SQLICRUD.getPayloadByID(3)
+
+    def test_create_correct_number_of_payloads(self):
+        self.assertEqual(2, len(self.__SQLICRUD.getSQLIPayloads()))
+
+    def test_update(self):
+        self.__SQLICRUD.updatePayload(SQLIPayloadEntity(2,'testUpdate','aa',2))
+        self.assertEqual('testUpdate', self.__SQLICRUD.getPayloadByID(2).getPayload())
+
+    def test_update_wrong_id(self):
+        with self.assertRaises(Exception) as cm:
+            self.__SQLICRUD.updatePayload(SQLIPayloadEntity(3,'a','b',2))
+        self.assertEqual(str(cm.exception), 'No such payload with id 3')
+
+    def test_delete_by_id(self):
+        self.__SQLICRUD.deletePayloadByID(1)
+        self.assertEqual(1, len(self.__SQLICRUD.getSQLIPayloads()))
+
+    def test_delete_all_data_from_table(self):
+        self.__SQLICRUD.deleteAllDataFromTable()
+        self.assertEqual(0, len(self.__SQLICRUD.getSQLIPayloads()))
+
+    def doCleanups(self):
+        pass
+
+    def suite(self):
+        pass
+
+
 
 if __name__ == '__main__':
     #unittest.main()
