081965c945efea8a76fd6fbf5ec4a4ada860b758
OrenZak <orenzak24@gmail.com>
2019-12-04 23:49:29 +0200

    main screen ui

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider } from '@material-ui/core';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import TrackChangesIcon from '@material-ui/icons/TrackChanges';
+import ReportIcon from '@material-ui/icons/Report';
+import AppLogo from './components/AppLogo'
+
+const drawerWidth = 240;
+const drawerTabsData = [{title: 'Scans', icon: <TrackChangesIcon/>}, {title:'Reports', icon: <ReportIcon/>}];
 
 const App: React.FC = () => {
   const classes = useStyles();
   return (
-    <AppBar position='static' className={classes.toolbar}>
-      <Toolbar>
-        <Typography variant='h6' className={classes.title}>
+    <div className={classes.root}>
+    <AppBar position='fixed' className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <AppLogo/>
+        <Typography variant='h6' noWrap className={classes.title}>
           Seccurate
         </Typography>
-        <SupervisedUserCircleIcon />
+        <SupervisedUserCircleIcon className={classes.manageButton}/>
       </Toolbar>
     </AppBar>
+    <Drawer
+    className={classes.drawer}
+    variant="permanent"
+    classes={{
+      paper: classes.drawerPaper,
+    }}
+  >
+    <div className={classes.toolbar} />
+    <List>
+      {drawerTabsData.map((item, index) => (
+        <ListItem button key={item.title}>
+          <ListItemIcon className={classes.tabColor}>{item.icon}</ListItemIcon>
+          <ListItemText className={classes.tabColor} primary={item.title} />
+          {/* if (index <= drawerTabsData.length) <Divider/> */}
+        </ListItem>
+      ))}
+    </List>
+  
+  </Drawer>
+  </div>
   );
 };
 
 const useStyles = makeStyles(theme => ({
+  toolbar: theme.mixins.toolbar,
   root: {
     flexGrow: 1
   },
@@ -28,8 +57,24 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1
   },
-  toolbar: {
-    backgroundColor: '#222831'
+  appBar: {
+    backgroundColor: '#ffffff',
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  manageButton: {
+    color: '#000000'
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: '#414141'
+  },
+  tabColor: {
+    color: '#ffffff'
   }
 }));
 
