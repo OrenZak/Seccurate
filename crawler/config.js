008b5f3c38a3fc55236a9f0f18557ea72723f1de
OrenZak <orenz@wix.com>
2019-12-18 11:25:12 +0200

    remove unused data

--- crawler/config.js
+++ crawler/config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    db_path: '../db',
-    crawler: {
-      'interval': 250, // 0.5 seconds
-      'maxConcurrency': 3,
-      'maxDepth': 10,
-      'timeout': 10000 // 10sec
-    },
-    paths: {
-      API_GETWAY: 'http://localhost:3001'
-    }
-}
+  crawler: {
+    interval: 500, // 0.5 seconds
+    maxConcurrency: 3,
+    maxDepth: 3,
+    timeout: 10000 // 10sec
+  },
+  paths: {
+    API_GATEWAY: 'http://localhost:3001'
+  }
+};
