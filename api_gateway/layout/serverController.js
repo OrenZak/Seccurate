1f3aac1ddd4aaec72bd23cfa0170a53261df2e96
Zur Ulianitzky <zur@xmcyber.com>
2020-02-29 11:29:18 +0200

    Asddsa

--- api_gateway/layout/serverController.js
+++ api_gateway/layout/serverController.js
@@ -10,7 +10,7 @@ http.listen(3001, function () {
 });
 
 let corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost',
     credentials: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
