9d743cda9ea8363627c3593fdec2bce387fa8327
Zur Ulianitzky <zur@xmcyber.com>
2020-02-29 11:17:01 +0200

    asd

--- api_gateway/layout/serverController.js
+++ api_gateway/layout/serverController.js
@@ -9,5 +9,10 @@ http.listen(3001, function() {
   console.log('listening on *:3001');
 });
 
-app.use(cors());
+let corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use('/', indexRouter.router);
